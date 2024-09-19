5c5eec9379bfaea0c172e9fe39f71bbe296771d7
irshadhussain21 <irshadhussain120@gmail.com>
2024-09-19 11:34:59 +0530

    initial commit

--- ILPB4_OJT_FRONTEND/src/app/app.component.ts
+++ ILPB4_OJT_FRONTEND/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarketlistComponent } from "./features/marketlist/marketlist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarketlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
