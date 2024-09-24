5f5bdf9b0af8c8015ce74f25052bc9723bc7c1cb
A-liya-s-alim <aliyasalim2001@gmail.com>
2024-09-24 09:35:56 +0530

    done

--- ILPB4_OJT_FRONTEND/src/app/features/view-market-details/view-market-details.component.ts
+++ ILPB4_OJT_FRONTEND/src/app/features/view-market-details/view-market-details.component.ts
@@ -44,19 +44,33 @@ import { ChipModule } from 'primeng/chip';
 
 import { PanelModule } from 'primeng/panel';
 import { TagModule } from 'primeng/tag';
-
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-view-market-details',
   standalone: true,
-  imports: [ CardModule, PanelModule, TagModule,ChipModule], // CommonModule for directives
+  imports: [ CardModule, PanelModule, TagModule,ChipModule,MenuModule,ButtonModule], // CommonModule for directives
   templateUrl: './view-market-details.component.html',
   styleUrl: './view-market-details.component.css'
 })
 export class ViewMarketDetailsComponent implements OnInit {
   marketDetails: any; // Replace with the appropriate type if available
+  items = [
+    { label: 'Activity Log', command: () => this.onActivityLog() },
+    { label: 'Delete Market', command: () => this.onDeleteMarket() }
+  ];
+  onActivityLog() {
+    // Handle activity log action
+    console.log('Activity Log clicked');
+  }
 
+  onDeleteMarket() {
+    // Handle delete market action
+    console.log('Delete Market clicked');
+  }
   ngOnInit() {
     // You can add your logic here
   }
+  
 }
