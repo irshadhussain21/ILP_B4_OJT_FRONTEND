697f95402f612c970d3f88fdf460a0cdba78a721
A-liya-s-alim <aliyasalim2001@gmail.com>
2024-10-10 13:42:05 +0530

    bp

--- ILPB4_OJT_FRONTEND/src/app/app.routes.ts
+++ ILPB4_OJT_FRONTEND/src/app/app.routes.ts
@@ -1,29 +1,34 @@
 import { Routes } from '@angular/router';
-import { MarketlistComponent } from './features/market-list/market-list.component'; 
+import { MarketlistComponent } from './features/market-list/market-list.component'; // Make sure to update the path
 import { CreateMarketComponent } from './features/create-market/create-market.component';
 import { ViewMarketDetailsComponent } from './features/view-market-details/view-market-details.component';
 import { EditMarketComponent } from './features/edit-market/edit-market.component';
+import { SubgroupComponent } from './features/subgroup/subgroup.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'markets',
-    pathMatch: 'full',
+    redirectTo: 'marketlist',
+    pathMatch: 'full'
   },
   {
-    path: 'markets',
-    component: MarketlistComponent,
+    path: 'marketlist',
+    component: MarketlistComponent
   },
   {
-    path: 'markets/create',
-    component: CreateMarketComponent,
+    path:"marketlist/create",
+    component:CreateMarketComponent
   },
   {
-    path: 'markets/edit/:id',
-    component: EditMarketComponent,
+    path: 'marketlist/edit/:id',
+    component:EditMarketComponent
   },
   {
-    path: 'markets/:marketId',
-    component: ViewMarketDetailsComponent,
+    path: 'marketlist/market/:marketId',
+    component:ViewMarketDetailsComponent
   },
+  {
+    path:'subgroup',
+    component: SubgroupComponent
+  }
 ];
